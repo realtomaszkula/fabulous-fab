c494039b4caa58b8ac53cd0de65ebd66374dec6d
Tomasz Kula <tomaszkula07@gmail.com>
2018-03-13 12:14:04 +0100

    Add bubble-child animations

--- src/app/fab/fab.animations.ts
+++ src/app/fab/fab.animations.ts
@@ -12,8 +12,52 @@ import {
   animateChild,
 } from '@angular/animations';
 
+// region timings
 const timing = '1000ms ease-out';
+// endregion
 
+// region constants
+const arch = 75;
+const height = 150;
+const baseTranslate = {
+  x: '-50%',
+  y: '-50%',
+};
+const children = {
+  child1: {
+    x: -50 + -height + '%',
+    y: -50 + arch + '%',
+  },
+  child2: {
+    x: -50 + 0 + '%',
+    y: -50 + height + '%',
+  },
+  child3: {
+    x: -50 + height + '%',
+    y: -50 + arch + '%',
+  },
+  child4: {
+    x: -50 + height + '%',
+    y: -50 + -arch + '%',
+  },
+  child5: {
+    x: -50 + 0 + '%',
+    y: -50 + -height + '%',
+  },
+  child6: {
+    x: -50 + -height + '%',
+    y: -50 + -arch + '%',
+  },
+};
+// endregion
+
+// region partial animations
+const transform = animation([
+  animate(timing, style({ transform: 'translate({{x}}, {{y}}' })),
+]);
+// endregion
+
+// region animations
 export const crossAnimation = trigger('cross', [
   state('on', style({ transform: 'rotate(270deg)' })),
   state('off', style({ transform: 'rotate(50deg)' })),
@@ -28,10 +72,173 @@ export const parentAnimation = trigger('parent', [
     group([
       style({ transform: 'scale(1)' }),
       animate(timing, style({ transform: 'scale(0.75)' })),
-      query('@cross', animateChild()),
+      query('@*', animateChild()),
     ]),
   ]),
   transition('on => off', [
-    group([animate(timing), query('@cross', animateChild())]),
+    group([animate(timing), query('@*', animateChild())]),
   ]),
 ]);
+
+export const childAnimation1 = trigger('child1', [
+  state(
+    'off',
+    style({
+      transform: `translate(${baseTranslate.x}, ${baseTranslate.y})`,
+    }),
+  ),
+  state(
+    'on',
+    style({
+      transform: `translate(${children.child1.x}, ${children.child1.y})`,
+    }),
+  ),
+  transition(
+    'off => on',
+    useAnimation(transform, {
+      params: children.child1,
+    }),
+  ),
+  transition(
+    'on => off',
+    useAnimation(transform, {
+      params: baseTranslate,
+    }),
+  ),
+]);
+
+export const childAnimation2 = trigger('child2', [
+  state(
+    'off',
+    style({
+      transform: `translate(${baseTranslate.x}, ${baseTranslate.y})`,
+    }),
+  ),
+  state(
+    'on',
+    style({
+      transform: `translate(${children.child2.x}, ${children.child2.y})`,
+    }),
+  ),
+  transition(
+    'off => on',
+    useAnimation(transform, {
+      params: children.child2,
+    }),
+  ),
+  transition(
+    'on => off',
+    useAnimation(transform, {
+      params: baseTranslate,
+    }),
+  ),
+]);
+
+export const childAnimation3 = trigger('child3', [
+  state(
+    'off',
+    style({
+      transform: `translate(${baseTranslate.x}, ${baseTranslate.y})`,
+    }),
+  ),
+  state(
+    'on',
+    style({
+      transform: `translate(${children.child3.x}, ${children.child3.y})`,
+    }),
+  ),
+  transition(
+    'off => on',
+    useAnimation(transform, {
+      params: children.child3,
+    }),
+  ),
+  transition(
+    'on => off',
+    useAnimation(transform, {
+      params: baseTranslate,
+    }),
+  ),
+]);
+
+export const childAnimation4 = trigger('child4', [
+  state(
+    'off',
+    style({
+      transform: `translate(${baseTranslate.x}, ${baseTranslate.y})`,
+    }),
+  ),
+  state(
+    'on',
+    style({
+      transform: `translate(${children.child4.x}, ${children.child4.y})`,
+    }),
+  ),
+  transition(
+    'off => on',
+    useAnimation(transform, {
+      params: children.child4,
+    }),
+  ),
+  transition(
+    'on => off',
+    useAnimation(transform, {
+      params: baseTranslate,
+    }),
+  ),
+]);
+
+export const childAnimation5 = trigger('child5', [
+  state(
+    'off',
+    style({
+      transform: `translate(${baseTranslate.x}, ${baseTranslate.y})`,
+    }),
+  ),
+  state(
+    'on',
+    style({
+      transform: `translate(${children.child5.x}, ${children.child5.y})`,
+    }),
+  ),
+  transition(
+    'off => on',
+    useAnimation(transform, {
+      params: children.child5,
+    }),
+  ),
+  transition(
+    'on => off',
+    useAnimation(transform, {
+      params: baseTranslate,
+    }),
+  ),
+]);
+
+export const childAnimation6 = trigger('child6', [
+  state(
+    'off',
+    style({
+      transform: `translate(${baseTranslate.x}, ${baseTranslate.y})`,
+    }),
+  ),
+  state(
+    'on',
+    style({
+      transform: `translate(${children.child6.x}, ${children.child6.y})`,
+    }),
+  ),
+  transition(
+    'off => on',
+    useAnimation(transform, {
+      params: children.child6,
+    }),
+  ),
+  transition(
+    'on => off',
+    useAnimation(transform, {
+      params: baseTranslate,
+    }),
+  ),
+]);
+// endregion
--- src/app/fab/fab.component.ts
+++ src/app/fab/fab.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { parentAnimation, crossAnimation } from './fab.animations';
+import {
+  parentAnimation,
+  crossAnimation,
+  childAnimation1,
+  childAnimation2,
+  childAnimation3,
+  childAnimation4,
+  childAnimation5,
+  childAnimation6,
+} from './fab.animations';
 
 enum BubbleState {
   on = 'on',
@@ -10,17 +19,26 @@ enum BubbleState {
   selector: 'zet-fab',
   template: `
   <div class="bubble bubble-parent" [@parent]="bubbleState" (click)="toggle()">
-    <div class="bubble bubble-child"></div>
-    <div class="bubble bubble-child"></div>
-    <div class="bubble bubble-child"></div>
-    <div class="bubble bubble-child"></div>
-    <div class="bubble bubble-child"></div>
-    <div class="bubble bubble-child"></div>
+    <div class="bubble bubble-child" [@child1]="bubbleState"></div>
+    <div class="bubble bubble-child" [@child2]="bubbleState"></div>
+    <div class="bubble bubble-child" [@child3]="bubbleState"></div>
+    <div class="bubble bubble-child" [@child4]="bubbleState"></div>
+    <div class="bubble bubble-child" [@child5]="bubbleState"></div>
+    <div class="bubble bubble-child" [@child6]="bubbleState"></div>
     <div class="bubble-cross" [@cross]="bubbleState">❌</div>
   </div>
   `,
   styleUrls: ['./fab.component.scss'],
-  animations: [parentAnimation, crossAnimation],
+  animations: [
+    parentAnimation,
+    crossAnimation,
+    childAnimation1,
+    childAnimation2,
+    childAnimation3,
+    childAnimation4,
+    childAnimation5,
+    childAnimation6,
+  ],
 })
 export class FabComponent implements OnInit {
   bubbleState: BubbleState = BubbleState.off;
